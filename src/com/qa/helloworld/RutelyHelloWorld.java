c01ccd52c420ff347185ffd92b6d414c9048f9cf
rcbcastillo <rcbcastillo@gmail.com>
2023-03-09 17:44:46 +0000

    Add src HelloWorld class

---
+++ src/com/qa/helloworld/RutelyHelloWorld.java
@@ -0,0 +1,14 @@
+package com.qa.helloworld;
+
+public class RutelyHelloWorld {
+
+	public static void printMessage() {
+		String salutation = "Hello World";
+		// telling java to print "Hello World"
+		System.out.println(salutation);
+	}
+
+	public static void main(String[] args) {
+		printMessage();
+	}
+}
